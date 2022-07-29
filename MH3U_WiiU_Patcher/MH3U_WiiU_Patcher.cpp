2ae3da8371ba750757af109bbfa168108efeffe5
Chantox <danielzegers22o@gmail.com>
2022-07-28 23:04:09 -0400

    Initial commit

---
+++ MH3U_WiiU_Patcher/MH3U_WiiU_Patcher.cpp
@@ -0,0 +1,13 @@
+﻿// MH3U_WiiU_Patcher.cpp : Defines the entry point for the application.
+//
+
+#include "MH3U_WiiU_Patcher.h"
+
+using namespace std;
+namespace fs = filesystem;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
